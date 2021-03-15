56463f312ec703bdfd3a237dd3611e2d981675f5
azizamari <azizamari2003@gmail.com>
2021-03-15 13:13:56 +0100

    Lexical Analyser Structure

--- Pital/Program.cs
+++ Pital/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Pital
 {
@@ -6,7 +7,128 @@ namespace Pital
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    return;
+                var lexer = new Lexer(line);
+                while (true)
+                {
+                    var token = lexer.NextToken();
+                    if (token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+                    Console.Write($"{token.Kind}: {token.Text}");
+                    if (token.Value != null)
+                        Console.Write($" {token.Value}");
+
+                    Console.WriteLine();
+                }
+            }
         }
     }
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhiteSpaceToken,
+        PlusToken,
+        StarToken,
+        MinusToken,
+        SlashToken,
+        OpenParenthesisToken,
+        ClosedParenthesisToken,
+        BadToken,
+        EndOfFileToken
+    }
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind,int position,string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text { get; }
+        public object Value { get; }
+    }
+    class Lexer
+    {
+        private readonly string _text;
+        private int _position;
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+
+        private char current
+        {
+            get
+            {
+                if (_position >= _text.Length)
+                    return '\0';
+                return _text[_position];
+            }
+        }
+        private void Next()
+        {
+            _position ++;
+        }
+
+        public SyntaxToken NextToken()
+        {
+            //numeric
+
+            //operators
+            //<ws>
+
+            if (_position >= _text.Length)
+            {
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+            }
+
+            if (char.IsDigit(current))
+            {
+                var start = _position;
+                while (char.IsDigit(current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text,value);
+            }
+
+            if (char.IsWhiteSpace(current))
+            {
+                var start = _position;
+                while (char.IsWhiteSpace(current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                return new SyntaxToken(SyntaxKind.WhiteSpaceToken, start, text, null);
+            }
+
+            if (current == '+')
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+            else if (current =='-')
+                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+            else if (current == '*')
+                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+            else if (current == '/')
+                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+            else if (current == '(')
+                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+            else if (current == ')')
+                return new SyntaxToken(SyntaxKind.ClosedParenthesisToken, _position++, ")", null);
+
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1,1), null);
+        }
+    }
+    
 }
