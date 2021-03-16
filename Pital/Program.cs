7a927604d58168307058d7fb3b5f5fd09ac6c3ad
azizamari <azizamari2003@gmail.com>
2021-03-16 10:36:13 +0100

    Initial Expression Parsing

--- Pital/Program.cs
+++ Pital/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Pital
 {
@@ -13,18 +14,31 @@ namespace Pital
                 var line = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(line))
                     return;
-                var lexer = new Lexer(line);
-                while (true)
-                {
-                    var token = lexer.NextToken();
-                    if (token.Kind == SyntaxKind.EndOfFileToken)
-                        break;
-                    Console.Write($"{token.Kind}: {token.Text}");
-                    if (token.Value != null)
-                        Console.Write($" {token.Value}");
 
-                    Console.WriteLine();
-                }
+                var parser = new Parser(line);
+                var expression = parser.Parse();
+
+                var color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                Print(expression);
+            }
+        }
+        static void Print(SyntaxNode node,string indent="")
+        {
+            Console.Write(indent);
+            Console.Write(node.Kind);
+            if(node is SyntaxToken T && T.Value != null)
+            {
+                Console.Write(" ");
+                Console.Write(T.Value);
+            }
+
+            Console.WriteLine();
+            indent += "    ";
+            foreach(var child in node.GetChildren())
+            {
+                Print(child, indent);
             }
         }
     }
@@ -39,9 +53,11 @@ namespace Pital
         OpenParenthesisToken,
         ClosedParenthesisToken,
         BadToken,
-        EndOfFileToken
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression
     }
-    class SyntaxToken
+    class SyntaxToken : SyntaxNode
     {
         public SyntaxToken(SyntaxKind kind,int position,string text, object value)
         {
@@ -51,10 +67,15 @@ namespace Pital
             Value = value;
         }
 
-        public SyntaxKind Kind { get; }
+        public override SyntaxKind Kind { get; }
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            return Enumerable.Empty<SyntaxNode>();
+        }
     }
     class Lexer
     {
@@ -130,5 +151,112 @@ namespace Pital
             return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1,1), null);
         }
     }
-    
+    abstract class SyntaxNode
+    {
+        public abstract SyntaxKind Kind { get; }
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+    abstract class ExpressionSyntax : SyntaxNode
+    {
+    }
+    sealed class NumberExpressionSyntax : ExpressionSyntax
+    {
+        public NumberExpressionSyntax(SyntaxToken numberToken)
+        {
+            NumberToken = numberToken;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.NumberExpression;
+
+        public SyntaxToken NumberToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return NumberToken;
+        }
+    }
+    sealed class BinaryExpressionSyntax : ExpressionSyntax
+    {
+        public BinaryExpressionSyntax(ExpressionSyntax left,SyntaxToken operatorToken, ExpressionSyntax right)
+        {
+            Left = left;
+            OperatorToken = operatorToken;
+            Right = right;
+        }
+
+        public ExpressionSyntax Left { get; }
+        public SyntaxToken OperatorToken { get; }
+        public ExpressionSyntax Right { get; }
+
+        public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Left;
+            yield return OperatorToken;
+            yield return Right;
+        }
+    }
+    class Parser
+    {
+        private readonly SyntaxToken[] _tokens;
+        private int _position;
+
+        public Parser(string text)
+        {
+            var tokens = new List<SyntaxToken>();
+
+            var lexer = new Lexer(text);
+            SyntaxToken token;
+            do
+            {
+                token = lexer.NextToken();
+                if (token.Kind != SyntaxKind.WhiteSpaceToken && token.Kind != SyntaxKind.BadToken)
+                {
+                    tokens.Add(token);
+                }
+            } while (token.Kind != SyntaxKind.EndOfFileToken);
+
+            _tokens = tokens.ToArray();
+        }
+
+        private SyntaxToken Peek(int offset)
+        {
+            var index = _position + offset;
+            if (index >= _tokens.Length)
+                return _tokens[_tokens.Length - 1];
+            return _tokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
+        private SyntaxToken NextToken()
+        {
+            var current = Current;
+            _position++;
+            return current;
+        }
+        private SyntaxToken Match(SyntaxKind kind)
+        {
+            if (Current.Kind == kind)
+                return NextToken();
+            return new SyntaxToken(kind, Current.Position, null, null);
+        }
+        public ExpressionSyntax Parse()
+        {
+            var left = ParsePrimaryExpression();
+            while (Current.Kind == SyntaxKind.PlusToken || Current.Kind == SyntaxKind.MinusToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParsePrimaryExpression();
+                left = new BinaryExpressionSyntax(left, operatorToken, right);
+            }
+            return left;
+        }
+
+        private ExpressionSyntax ParsePrimaryExpression()
+        {
+            var numberToken = Match(SyntaxKind.NumberToken);
+            return new NumberExpressionSyntax(numberToken);
+        }
+    }
 }
