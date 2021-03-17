7d0ab54ca4c657140a0b42630e406c7b7ca2b8da
azizamari <azizamari2003@gmail.com>
2021-03-17 18:46:55 +0100

    Added Expression Evaluation and () Support

--- Pital/Program.cs
+++ Pital/Program.cs
@@ -8,45 +8,77 @@ namespace Pital
     {
         static void Main(string[] args)
         {
+            bool showTree = false;
             while (true)
             {
                 Console.Write("> ");
                 var line = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(line))
                     return;
+                if (line == "#showTree")
+                {
+                    showTree = !showTree;
+                    Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees");
+                    continue;
+                }
+                else if (line == "#cls")
+                {
+                    Console.Clear();
+                    continue;
+                }
 
-                var parser = new Parser(line);
-                var expression = parser.Parse();
+                var syntaxTree = SyntaxTree.Parse(line);
 
                 var color = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.DarkGray;
+                if (showTree)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    TreePrint(syntaxTree.Root);
+                    Console.ForegroundColor = color;
+                }
 
-                TreePrint(expression);
+                if (!syntaxTree.Diagnostics.Any())
+                {
+                    var e = new Evaluator(syntaxTree.Root);
+                    var result = e.Evaluate();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    foreach (var diagnostic in syntaxTree.Diagnostics)
+                    {
+                        Console.WriteLine(diagnostic);
+                    }
+
+                    Console.ForegroundColor = color;
+                }
             }
         }
         // ├──
         // │    
         // └──
-        static void TreePrint(SyntaxNode node,string indent="", bool isLast=true)
+        static void TreePrint(SyntaxNode node, string indent = "", bool isLast = true)
         {
             var marker = isLast ? "└──" : "├──";
             Console.Write(indent);
             Console.Write(marker);
             Console.Write(node.Kind);
-            if(node is SyntaxToken T && T.Value != null)
+            if (node is SyntaxToken T && T.Value != null)
             {
                 Console.Write(" ");
                 Console.Write(T.Value);
             }
 
             Console.WriteLine();
-            indent +=isLast?"   ": "│  ";
+            indent += isLast ? "   " : "│  ";
 
             var lastChild = node.GetChildren().LastOrDefault();
 
-            foreach(var child in node.GetChildren())
+            foreach (var child in node.GetChildren())
             {
-                TreePrint(child, indent,child ==lastChild);
+                TreePrint(child, indent, child == lastChild);
             }
         }
     }
@@ -63,11 +95,12 @@ namespace Pital
         BadToken,
         EndOfFileToken,
         NumberExpression,
-        BinaryExpression
+        BinaryExpression,
+        ParenthesizedExpression
     }
     class SyntaxToken : SyntaxNode
     {
-        public SyntaxToken(SyntaxKind kind,int position,string text, object value)
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
         {
             Kind = kind;
             Position = position;
@@ -89,11 +122,14 @@ namespace Pital
     {
         private readonly string _text;
         private int _position;
+        private List<string> _diagnostics = new List<string>();
+
         public Lexer(string text)
         {
             _text = text;
         }
 
+        public IEnumerable<string> Diagnostics => _diagnostics;
         private char current
         {
             get
@@ -105,7 +141,7 @@ namespace Pital
         }
         private void Next()
         {
-            _position ++;
+            _position++;
         }
 
         public SyntaxToken NextToken()
@@ -128,8 +164,11 @@ namespace Pital
 
                 var length = _position - start;
                 var text = _text.Substring(start, length);
-                int.TryParse(text, out var value);
-                return new SyntaxToken(SyntaxKind.NumberToken, start, text,value);
+                if (!int.TryParse(text, out var value))
+                {
+                    _diagnostics.Add($"The number {_text} isn't valid Int32");
+                }
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
             }
 
             if (char.IsWhiteSpace(current))
@@ -145,7 +184,7 @@ namespace Pital
 
             if (current == '+')
                 return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
-            else if (current =='-')
+            else if (current == '-')
                 return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
             else if (current == '*')
                 return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
@@ -156,7 +195,8 @@ namespace Pital
             else if (current == ')')
                 return new SyntaxToken(SyntaxKind.ClosedParenthesisToken, _position++, ")", null);
 
-            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1,1), null);
+            _diagnostics.Add($"ERROR: bad character input: {current}");
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
         }
     }
     abstract class SyntaxNode
@@ -185,7 +225,7 @@ namespace Pital
     }
     sealed class BinaryExpressionSyntax : ExpressionSyntax
     {
-        public BinaryExpressionSyntax(ExpressionSyntax left,SyntaxToken operatorToken, ExpressionSyntax right)
+        public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxToken operatorToken, ExpressionSyntax right)
         {
             Left = left;
             OperatorToken = operatorToken;
@@ -205,9 +245,52 @@ namespace Pital
             yield return Right;
         }
     }
+    sealed class ParenthesizedExpressionSyntax : ExpressionSyntax
+    {
+        public ParenthesizedExpressionSyntax(SyntaxToken openParenthesisToken, ExpressionSyntax expression, SyntaxToken closedParenthesisToken)
+        {
+            OpenParenthesisToken = openParenthesisToken;
+            Expression = expression;
+            ClosedParenthesisToken = closedParenthesisToken;
+        }
+
+
+        public override SyntaxKind Kind =>SyntaxKind.ParenthesizedExpression;
+        public SyntaxToken OpenParenthesisToken { get; }
+        public ExpressionSyntax Expression { get; }
+        public SyntaxToken ClosedParenthesisToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return OpenParenthesisToken;
+            yield return Expression;
+            yield return ClosedParenthesisToken;
+        }
+    }
+    sealed class SyntaxTree
+    {
+        public SyntaxTree(IEnumerable<string> diagnostics,ExpressionSyntax root,SyntaxToken endOfFileToken)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndOfFileToken = endOfFileToken;
+        }
+
+        public IReadOnlyList<string> Diagnostics { get; }
+        public ExpressionSyntax Root { get; }
+        public SyntaxToken EndOfFileToken { get; }
+
+        public static SyntaxTree Parse(string text)
+        {
+            var parser = new Parser(text);
+            return parser.Parse();
+        }
+    }
     class Parser
     {
         private readonly SyntaxToken[] _tokens;
+
+        private List<string> _diagnostics = new List<string>();
         private int _position;
 
         public Parser(string text)
@@ -226,7 +309,10 @@ namespace Pital
             } while (token.Kind != SyntaxKind.EndOfFileToken);
 
             _tokens = tokens.ToArray();
+            _diagnostics.AddRange(lexer.Diagnostics);
         }
+
+        public IEnumerable<string> Diagnostics => _diagnostics;
 
         private SyntaxToken Peek(int offset)
         {
@@ -247,12 +333,38 @@ namespace Pital
         {
             if (Current.Kind == kind)
                 return NextToken();
+            _diagnostics.Add($"ERROR: unexpected token <{Current.Kind}>, expected <{kind}>");
             return new SyntaxToken(kind, Current.Position, null, null);
         }
-        public ExpressionSyntax Parse()
+
+        private ExpressionSyntax ParseExpression()
+        {
+            return ParseTerm();
+        }
+
+        public SyntaxTree Parse()
+        {
+            var expression= ParseTerm();
+            var endOfFileToken=Match(SyntaxKind.EndOfFileToken);
+            return new SyntaxTree(Diagnostics, expression, endOfFileToken);
+        }
+
+        private ExpressionSyntax ParseTerm()
+        {
+            var left = ParseFactor();
+            while (Current.Kind == SyntaxKind.PlusToken || Current.Kind == SyntaxKind.MinusToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParseFactor();
+                left = new BinaryExpressionSyntax(left, operatorToken, right);
+            }
+            return left;
+        }
+
+        private ExpressionSyntax ParseFactor()
         {
             var left = ParsePrimaryExpression();
-            while (Current.Kind == SyntaxKind.PlusToken || Current.Kind == SyntaxKind.MinusToken)
+            while (Current.Kind == SyntaxKind.StarToken || Current.Kind == SyntaxKind.SlashToken)
             {
                 var operatorToken = NextToken();
                 var right = ParsePrimaryExpression();
@@ -263,8 +375,61 @@ namespace Pital
 
         private ExpressionSyntax ParsePrimaryExpression()
         {
+            if (Current.Kind == SyntaxKind.OpenParenthesisToken)
+            {
+                var left = NextToken();
+                var expression = ParseExpression();
+                var right = Match(SyntaxKind.ClosedParenthesisToken);
+                return new ParenthesizedExpressionSyntax(left,expression,right);
+            }
+
             var numberToken = Match(SyntaxKind.NumberToken);
             return new NumberExpressionSyntax(numberToken);
+        }
+    }
+    class Evaluator
+    {
+        private readonly ExpressionSyntax _root;
+
+        public Evaluator(ExpressionSyntax root)
+        {
+            _root = root;
+        }
+        public int Evaluate()
+        {
+            return EvaluateExpression(_root);
+        }
+
+        private int EvaluateExpression(ExpressionSyntax node)
+        {
+            //binary
+            //number
+            if (node is NumberExpressionSyntax n)
+            {
+                return (int)n.NumberToken.Value;
+            }
+            if(node is BinaryExpressionSyntax b)
+            {
+                var left = EvaluateExpression(b.Left);
+                var right = EvaluateExpression(b.Right);
+                if (b.OperatorToken.Kind == SyntaxKind.PlusToken)
+                    return left + right;
+                else if (b.OperatorToken.Kind == SyntaxKind.MinusToken)
+                    return left - right;
+                else if (b.OperatorToken.Kind == SyntaxKind.StarToken)
+                    return left * right;
+                else if (b.OperatorToken.Kind == SyntaxKind.SlashToken)
+                    return left / right;
+                else if (b.OperatorToken.Kind == SyntaxKind.MinusToken)
+                    return left - right;
+                else
+                    throw new Exception($"Unexpected Operator {b.OperatorToken.Kind}");
+            }
+            if(node is ParenthesizedExpressionSyntax p)
+            {
+                return EvaluateExpression(p.Expression);
+            }
+            throw new Exception($"Unexpected node {node.Kind}");
         }
     }
 }
