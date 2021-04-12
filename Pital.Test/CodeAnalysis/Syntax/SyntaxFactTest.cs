2209ac44cdc075f7f978b6a5fba8f99e3a873de3
azizamari <azizamari2003@gmail.com>
2021-04-12 22:14:06 +0100

    Add SyntaxFacts testing

---
+++ Pital.Test/CodeAnalysis/Syntax/SyntaxFactTest.cs
@@ -0,0 +1,34 @@
+﻿using Pital.CodeAnalysis.Syntax;
+using System;
+using Xunit;
+using System.Collections.Generic;
+
+namespace Pital.Test.CodeAnalysis.Syntax
+{
+    public class SyntaxFactTest
+    {
+        [Theory]
+        [MemberData(nameof(GetSyntaxKindData))]
+        public void SyntaxFact_GetText_RoundTrips(SyntaxKind kind)
+        {
+            var text = SyntaxFacts.GetText(kind);
+            if (text == null)
+                return;
+
+            var tokens = SyntaxTree.ParseTokens(text);
+            var token = Assert.Single(tokens);
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text);
+        }
+        
+        public static IEnumerable<object[]> GetSyntaxKindData()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+            foreach(var kind in kinds)
+            {
+                yield return new object[] { kind };
+            }
+        }
+    }
+}
+
