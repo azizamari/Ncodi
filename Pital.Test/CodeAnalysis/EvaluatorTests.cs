266d9338154065c5c83aeadf9dcc0d79a1b67dcc
azizamari <azizamari2003@gmail.com>
2021-04-13 20:16:31 +0100

    Created evaluation tests

---
+++ Pital.Test/CodeAnalysis/EvaluatorTests.cs
@@ -0,0 +1,47 @@
+﻿using Pital.CodeAnalysis;
+using Pital.CodeAnalysis.Syntax;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Pital.Test.CodeAnalysis
+{
+    public class EvaluatorTests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("+1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("14 + 12", 26)]
+        [InlineData("12 - 3", 9)]
+        [InlineData("4 * 2", 8)]
+        [InlineData("9 / 3", 3)]
+        [InlineData("(10)", 10)]
+        [InlineData("12 == 3", false)]
+        [InlineData("3 == 3", true)]
+        [InlineData("12 != 3", true)]
+        [InlineData("3 != 3", false)]
+        [InlineData("false == false", true)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        [InlineData("true == false", false)]
+        [InlineData("false != false", false)]
+        [InlineData("true != false", true)]
+        [InlineData("true & true", true)]
+        [InlineData("false | false", false)]
+        [InlineData("true | false", true)]
+        [InlineData("true | true", true)]
+        [InlineData("false & false", false)]
+        [InlineData("false & true", false)]
+        [InlineData("true & false", false)]
+        [InlineData("(a=10)*a", 100)]
+        public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+            var compilation = new Compilation(syntaxTree);
+            var variables = new Dictionary<VariableSymbol, object>();
+            var result = compilation.Evaluate(variables);
+            Assert.Empty(result.Diagnostics);
+            Assert.Equal(expectedValue, result.Value);
+        }
+    }
+}
