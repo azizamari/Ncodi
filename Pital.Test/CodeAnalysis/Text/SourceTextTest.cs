2c9f42c6e1203a2d73db8d0cea1a3ea760c0e3d3
azizamari <azizamari2003@gmail.com>
2021-04-18 17:05:22 +0100

    Created Tests to ensure the number of lines in SourceText is as expected

---
+++ Pital.Test/CodeAnalysis/Text/SourceTextTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Pital.CodeAnalysis.Text;
+
+namespace Pital.Test.CodeAnalysis.Text
+{
+    public class SourceTextTest
+    {
+        [Theory]
+        [InlineData(".",1)]
+        [InlineData(".\r\n",2)]
+        [InlineData(".\r\n\r\n", 3)]
+        public void SourceText_IncludesLastLine(string text,int expectedLineCount)
+        {
+            var sourceText = SourceText.From(text);
+            var actualLineCount = sourceText.Lines.Length;
+            Assert.Equal(expectedLineCount, actualLineCount);
+        }
+    }
+}
+
