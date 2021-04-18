048c8ff5c89ff5c5d222e6db0166b61aefe6662e
azizamari <azizamari2003@gmail.com>
2021-04-18 16:02:04 +0100

    Made sure that the last line gets added to SourceText

--- Pital/CodeAnalysis/Text/SourceText.cs
+++ Pital/CodeAnalysis/Text/SourceText.cs
@@ -59,7 +59,7 @@ namespace Pital.CodeAnalysis.Text
                 }
             }
 
-            if(position > lineStart)
+            if(position >= lineStart)
                 AddLine(result ,sourceText, position, lineStart, 0);
 
             return result.ToImmutable();
