234b60eb2b571e27477954952118d8ca2d4a8e67
azizamari <azizamari2003@gmail.com>
2021-04-25 07:00:54 +0100

    Fix access identifiers for methods in BoundTreeWriter

--- Pital/CodeAnalysis/Binding/BoundTreeRewriter.cs
+++ Pital/CodeAnalysis/Binding/BoundTreeRewriter.cs
@@ -26,7 +26,7 @@ namespace Pital.CodeAnalysis.Binding
             }
         }
 
-        private BoundStatement RewriteBlockStatement(BoundBlockStatement node)
+        protected virtual BoundStatement RewriteBlockStatement(BoundBlockStatement node)
         {
             ImmutableArray<BoundStatement>.Builder builder = null;
 
@@ -55,7 +55,7 @@ namespace Pital.CodeAnalysis.Binding
             return new BoundBlockStatement(builder.MoveToImmutable()); 
         }
 
-        private BoundStatement RewriteVariableDeclaration(BoundVariableDeclaration node)
+        protected virtual BoundStatement RewriteVariableDeclaration(BoundVariableDeclaration node)
         {
             var initializer = RewriteExpression(node.Initializer);
             if (initializer == node.Initializer)
@@ -64,7 +64,7 @@ namespace Pital.CodeAnalysis.Binding
             return new BoundVariableDeclaration(node.Variable, initializer);
         }
 
-        private BoundStatement RewriteIfStatement(BoundIfStatement node)
+        protected virtual BoundStatement RewriteIfStatement(BoundIfStatement node)
         {
             var condition = RewriteExpression(node.Condition);
             var thenStatement = RewriteStatement(node.ThenStatement);
@@ -75,7 +75,7 @@ namespace Pital.CodeAnalysis.Binding
             return new BoundIfStatement(condition, thenStatement, elseStatement);
         }
 
-        private BoundStatement RewriteWhileStatement(BoundWhileStatement node)
+        protected virtual BoundStatement RewriteWhileStatement(BoundWhileStatement node)
         {
             var condition = RewriteExpression(node.Condition);
             var body = RewriteStatement(node.Body);
@@ -85,7 +85,7 @@ namespace Pital.CodeAnalysis.Binding
             return new BoundWhileStatement(condition, body);
         }
 
-        private BoundStatement RewriteForStatement(BoundForStatement node)
+        protected virtual BoundStatement RewriteForStatement(BoundForStatement node)
         {
             var lowerBound = RewriteExpression(node.LowerBound);
             var upperBound = RewriteExpression(node.UpperBound);
@@ -96,7 +96,7 @@ namespace Pital.CodeAnalysis.Binding
             return new BoundForStatement(node.Variable, lowerBound, upperBound, body);
         }
 
-        private BoundStatement RewriteExpressionStatement(BoundExpressionStatement node)
+        protected virtual BoundStatement RewriteExpressionStatement(BoundExpressionStatement node)
         {
             var expression = RewriteExpression(node.Expression);
             if (expression == node.Expression)
