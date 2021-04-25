c69a674097f04e754b17a2993f5846c124f343d5
azizamari <azizamari2003@gmail.com>
2021-04-25 20:55:52 +0100

    Fully reworked the REPL Fixed #6

--- src/Pital/CodeAnalysis/Syntax/SyntaxToken.cs
+++ src/Pital/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -19,5 +19,6 @@ namespace Pital.CodeAnalysis.Syntax
         public string Text { get; }
         public object Value { get; }
         public override TextSpan Span => new TextSpan(Position,Text?.Length??0);
+        public bool IsMissing => Text == null;
     }
 }
