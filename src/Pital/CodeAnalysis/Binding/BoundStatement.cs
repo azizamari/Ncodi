6dc9a4117029e0c2a6b1e6a47990a95fe19f5da5
azizamari <azizamari2003@gmail.com>
2021-04-25 15:12:22 +0100

    Move BoundVariableDeclaration to new file

--- src/Pital/CodeAnalysis/Binding/BoundStatement.cs
+++ src/Pital/CodeAnalysis/Binding/BoundStatement.cs
@@ -4,16 +4,4 @@
     {
 
     }
-    internal sealed class BoundVariableDeclaration : BoundStatement
-    {
-        public BoundVariableDeclaration(VariableSymbol variable, BoundExpression initializer)
-        {
-            Variable = variable;
-            Initializer = initializer;
-        }
-        public VariableSymbol Variable { get; }
-        public BoundExpression Initializer { get; }
-
-        public override BoundNodeKind Kind => BoundNodeKind.VariableDeclaration;
-    }
 }
---
+++ src/Pital/CodeAnalysis/Binding/BoundVariableDeclaration.cs
@@ -0,0 +1,15 @@
+﻿namespace Pital.CodeAnalysis.Binding
+{
+    internal sealed class BoundVariableDeclaration : BoundStatement
+    {
+        public BoundVariableDeclaration(VariableSymbol variable, BoundExpression initializer)
+        {
+            Variable = variable;
+            Initializer = initializer;
+        }
+        public VariableSymbol Variable { get; }
+        public BoundExpression Initializer { get; }
+
+        public override BoundNodeKind Kind => BoundNodeKind.VariableDeclaration;
+    }
+}
