02cb6836fbb4af56ae139b96cea1e80e95bc079b
azizamari <azizamari2003@gmail.com>
2021-04-25 17:29:55 +0100

    Seperate concerncs in program.cs by creating an abstract Repl class and PitalRepl

--- src/Pc/Program.cs
+++ src/Pc/Program.cs
@@ -9,21 +9,16 @@ using System.Text;
 
 namespace Pc
 {
-    internal static class Program
+    internal abstract class Repl
     {
-        private static void Main()
-        {
-            
-            var showTree = false;
-            var showProgram = false;
-            var variables = new Dictionary<VariableSymbol, object>();
-            var textBuilder = new StringBuilder();
-            Compilation previous = null;
+        private StringBuilder _textBuilder =new StringBuilder();
 
+        public void Run()
+        {
             while (true)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                if (textBuilder.Length == 0)
+                if (_textBuilder.Length == 0)
                     Console.Write("» ");
                 else
                     Console.Write("· ");
@@ -31,95 +26,144 @@ namespace Pc
                 var input = Console.ReadLine();
                 var isBlank = string.IsNullOrWhiteSpace(input);
 
-                if (textBuilder.Length == 0)
+                if (_textBuilder.Length == 0)
                 {
                     if (isBlank)
                     {
                         break;
                     }
+                    else if (input.StartsWith('#'))
+                    {
+                        EvaluateMetaCommand(input);
+                        continue;
+                    }
 
-                    else if (input == "#showTree")
-                    {
-                        showTree = !showTree;
-                        Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees");
-                        continue;
-                    }
-                    else if (input == "#showProgram")
-                    {
-                        showProgram = !showProgram;
-                        Console.WriteLine(showProgram ? "Showing bound tree" : "Not showing boud tree");
-                        continue;
-                    }
-                    else if (input == "#cls")
-                    {
-                        Console.Clear();
-                        continue;
-                    }
-                    else if (input == "#reset")
-                    {
-                        previous = null;
-                        continue;
-                    }
                 }
 
-                textBuilder.AppendLine(input);
-                var text = textBuilder.ToString();
-
-                var syntaxTree = SyntaxTree.Parse(text);
-                if (!isBlank && syntaxTree.Diagnostics.Any())
-                {
+                _textBuilder.AppendLine(input);
+                var text = _textBuilder.ToString();
+                if (!IsCompleteSubmission(text))
                     continue;
-                }
-
-                var compilation = previous ==null? new Compilation(syntaxTree):previous.ContinueWith(syntaxTree);
-                var result = compilation.Evaluate(variables);
-
-                if (showTree)
-                    syntaxTree.Root.WriteTo(Console.Out);
-                    Console.ResetColor();
-                if (showProgram)
-                    compilation.EmitTree(Console.Out);
-
-                if (!result.Diagnostics.Any())
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(result.Value);
-                    Console.ResetColor();
-
-                    previous = compilation;
-                }
-                else
-                {
-                    foreach (var diagnostic in result.Diagnostics)
-                    {
-                        var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
-                        var line = syntaxTree.Text.Lines[lineIndex];
-                        var lineNumber = lineIndex + 1;
-                        var character = diagnostic.Span.Start - line.Start+1;
-
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write($"({lineNumber}, {character}): ");
-                        Console.WriteLine(diagnostic);
-                        Console.ResetColor();
-
-                        var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
-                        var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End,line.End);
-
-                        var prefix = syntaxTree.Text.ToString(prefixSpan);
-                        var error = syntaxTree.Text.ToString(diagnostic.Span);
-                        var suffix= syntaxTree.Text.ToString(suffixSpan);
-
-                        Console.Write("    ");
-                        Console.WriteLine(prefix+error+suffix);
-                        var arrows = "    " +new string(' ',prefix.Length)+ new string('^', error.Length);
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine(arrows);
-                        Console.ResetColor();
-                    }
-
-                }
-                textBuilder.Clear();
+                EvaluateSubmission(text);
+                _textBuilder.Clear();
             }
+        }
+        protected virtual void EvaluateMetaCommand(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Invalid command {input}.");
+            Console.ResetColor();
+        }
+
+        protected abstract bool IsCompleteSubmission(string text);
+
+        protected abstract void EvaluateSubmission(string text);
+    }
+
+    internal sealed class PitalRepl : Repl
+    {
+        private Compilation _previous;
+        private bool _showTree = false;
+        private bool _showProgram = false;
+        private readonly Dictionary<VariableSymbol, object> _variables = new Dictionary<VariableSymbol, object>();
+
+        protected override void EvaluateMetaCommand(string input)
+        {
+            switch (input)
+            {
+                case "#showTree":
+                    _showTree = !_showTree;
+                    Console.WriteLine(_showTree ? "Showing parse trees." : "Not showing parse trees");
+                    break;
+                case "#showProgram":
+                    _showProgram = !_showProgram;
+                    Console.WriteLine(_showProgram ? "Showing bound tree" : "Not showing boud tree");
+                    break;
+                case "#cls":
+                    Console.Clear();
+                    break;
+                case "#reset":
+                    _previous = null;
+                    break;
+                default:
+                    base.EvaluateMetaCommand(input);
+                    break;
+            }
+        }
+
+        protected override void EvaluateSubmission(string text)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+
+
+            var compilation = _previous == null ? new Compilation(syntaxTree) : _previous.ContinueWith(syntaxTree);
+            var result = compilation.Evaluate(_variables);
+
+            if (_showTree)
+                syntaxTree.Root.WriteTo(Console.Out);
+            Console.ResetColor();
+            if (_showProgram)
+                compilation.EmitTree(Console.Out);
+
+            if (!result.Diagnostics.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(result.Value);
+                Console.ResetColor();
+
+                _previous = compilation;
+            }
+            else
+            {
+                foreach (var diagnostic in result.Diagnostics)
+                {
+                    var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+                    var line = syntaxTree.Text.Lines[lineIndex];
+                    var lineNumber = lineIndex + 1;
+                    var character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"({lineNumber}, {character}): ");
+                    Console.WriteLine(diagnostic);
+                    Console.ResetColor();
+
+                    var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                    var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                    var prefix = syntaxTree.Text.ToString(prefixSpan);
+                    var error = syntaxTree.Text.ToString(diagnostic.Span);
+                    var suffix = syntaxTree.Text.ToString(suffixSpan);
+
+                    Console.Write("    ");
+                    Console.WriteLine(prefix + error + suffix);
+                    var arrows = "    " + new string(' ', prefix.Length) + new string('^', error.Length);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(arrows);
+                    Console.ResetColor();
+                }
+
+            }
+
+            //return _previous;
+        }
+
+        protected override bool IsCompleteSubmission(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+            var syntaxTree = SyntaxTree.Parse(text);
+            if (syntaxTree.Diagnostics.Any())
+                return false;
+            return true;
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var repl = new PitalRepl();
+            repl.Run();
         }
     }
 }
