9f7cbd4ebeb12db37d30b248051ba1f5e610356c
azizamari <azizamari2003@gmail.com>
2021-04-26 11:26:10 +0100

    Stop taking input after user presses enter 2 times and removed GetLastToken because it crashes the program

--- src/Pc/PitalRepl.cs
+++ src/Pc/PitalRepl.cs
@@ -63,22 +63,28 @@ namespace Pc
             if (string.IsNullOrEmpty(text))
                 return true;
 
+
+            // not sure about this
+            if (text.EndsWith("\r\n\r\n"))
+                return true;
+
             var syntaxTree = SyntaxTree.Parse(text);
 
-            if (GetLastToken(syntaxTree.Root.Statement).IsMissing)
-                return false;
+            //if (GetLastToken(syntaxTree.Root.Statement).IsMissing)
+            if (syntaxTree.Diagnostics.Any())
+                    return false;
 
             return true;
         }
 
-        private static SyntaxToken GetLastToken(SyntaxNode node)
-        {
-            if (node is SyntaxToken token)
-                return token;
+        //private static SyntaxToken GetLastToken(SyntaxNode node)
+        //{
+        //    if (node is SyntaxToken token)
+        //        return token;
 
-            // A syntax node should always contain at least 1 token.
-            return GetLastToken(node.GetChildren().Last());
-        }
+        //    // A syntax node should always contain at least 1 token.
+        //    return GetLastToken(node.GetChildren().Last());
+        //}
 
         protected override void EvaluateSubmission(string text)
         {
