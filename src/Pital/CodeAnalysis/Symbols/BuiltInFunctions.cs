905ffe3fa8722a79b631ac18df2a48381dc148f9
azizamari <azizamari2003@gmail.com>
2021-04-29 16:13:06 +0100

    Add commented Tunisian syntax of print and input, respectivly ekteb and a9ra.

--- src/Pital/CodeAnalysis/Symbols/BuiltInFunctions.cs
+++ src/Pital/CodeAnalysis/Symbols/BuiltInFunctions.cs
@@ -7,6 +7,9 @@ namespace Pital.CodeAnalysis.Symbols
 {
     internal static class BuiltInFunctions
     {
+        //public static readonly FunctionSymbol Print=new FunctionSymbol("ekteb", ImmutableArray.Create(new ParameterSymbol("text", TypeSymbol.String)),TypeSymbol.Void);
+        //public static readonly FunctionSymbol Input=new FunctionSymbol("a9ra", ImmutableArray<ParameterSymbol>.Empty,TypeSymbol.String);
+
         public static readonly FunctionSymbol Print=new FunctionSymbol("print", ImmutableArray.Create(new ParameterSymbol("text", TypeSymbol.String)),TypeSymbol.Void);
         public static readonly FunctionSymbol Input=new FunctionSymbol("input", ImmutableArray<ParameterSymbol>.Empty,TypeSymbol.String);
         internal static IEnumerable<FunctionSymbol> GetAll() => typeof(BuiltInFunctions).GetFields(BindingFlags.Public | BindingFlags.Static)
