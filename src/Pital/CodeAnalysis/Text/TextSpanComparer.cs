4e173934c0f9835b243bae01f33fb82731876e84
azizamari <azizamari2003@gmail.com>
2021-05-04 10:08:21 +0100

    Add comparer so I can order diagnostic messages by their textSpan's

---
+++ src/Pital/CodeAnalysis/Text/TextSpanComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Pital.CodeAnalysis.Text
+{
+    public class TextSpanComparer : IComparer<TextSpan>
+    {
+        public int Compare(TextSpan x, TextSpan y)
+        {
+            int cmp = x.Start - y.Start;
+            if (cmp == 0)
+                cmp = x.Length - y.Length;
+            return cmp;
+        }
+    }
+}
