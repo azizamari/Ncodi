15413c24818727259c15933a07087ad62453d3b9
azizamari <azizamari2003@gmail.com>
2021-05-09 15:27:35 +0100

    Add ability to elegantly generate the Control Flow Graph

--- src/Pital/CodeAnalysis/Binding/ControlFlowGraph.cs
+++ src/Pital/CodeAnalysis/Binding/ControlFlowGraph.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ncodi.CodeAnalysis.Symbols;
+using Ncodi.CodeAnalysis.Syntax;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -123,24 +125,95 @@ namespace Ncodi.CodeAnalysis.Binding
         }
         public sealed class GraphBuilder
         {
+            private Dictionary<BoundStatement, BasicBlock> _blockFromStatement = new Dictionary<BoundStatement, BasicBlock>();
+            private Dictionary<BoundLabel, BasicBlock> _blockFromLabel = new Dictionary<BoundLabel, BasicBlock>();
             private List<BasicBlockBranch> _branches = new List<BasicBlockBranch>();
+            private BasicBlock _start = new BasicBlock(isStart: true);
+            private BasicBlock _end = new BasicBlock(isStart: false);
             public ControlFlowGraph Build(List<BasicBlock> blocks)
             {
-                var start = new BasicBlock(isStart: true);
-                var end = new BasicBlock(isStart: false);
                 if (!blocks.Any())
-                    Connect(start, end);
+                    Connect(_start, _end);
                 else
-                    Connect(start, blocks.First());
+                    Connect(_start, blocks.First());
 
-                blocks.Insert(0, start);
-                blocks.Add(end);
+                foreach (var block in blocks)
+                {
+                    foreach (var statement in block.Statements)
+                    {
+                        _blockFromStatement.Add(statement, block);
+                        if (statement is BoundLabelStatement labelStatement)
+                            _blockFromLabel.Add(labelStatement.Label, block);
+                    }
+                }
 
-                return new ControlFlowGraph(start, end, blocks, _branches);
+                for (int i = 0; i < blocks.Count; i++)
+                {
+                    var current = blocks[i];
+                    var next = i == blocks.Count - 1 ? _end : blocks[i + 1];
+                    foreach (var statement in current.Statements)
+                    {
+                        var isLastStatementInBlock = statement == current.Statements.Last();
+                        switch (statement.Kind)
+                        {
+                            case BoundNodeKind.GotoStatement:
+                                var gs = (BoundGotoStatement)statement;
+                                var toBlock = _blockFromLabel[gs.Label];
+                                Connect(current, toBlock);
+                                break;
+                            case BoundNodeKind.ConditionalGotoStatement:
+                                var cgs = (BoundConditionalGotoStatement)statement;
+                                var thenBlock = _blockFromLabel[cgs.Label];
+                                var elseBlock = next;
+                                var negatedCondition = Negate(cgs.Condition); ;
+                                var thenCondition = cgs.JumpIfTrue ? cgs.Condition : negatedCondition;
+                                var elseCondition = cgs.JumpIfTrue ? negatedCondition : cgs.Condition;
+                                Connect(current, thenBlock, thenCondition);
+                                Connect(current, elseBlock, elseCondition);
+                                break;
+                            case BoundNodeKind.ReturnStatement:
+                                Connect(current, _end);
+                                break;
+                            case BoundNodeKind.VariableDeclaration:
+                            case BoundNodeKind.LabelStatement:
+                            case BoundNodeKind.ExpressionStatement:
+                                if (isLastStatementInBlock)
+                                    Connect(current, next);
+                                break;
+                            default:
+                                throw new Exception($"Unexpected statement: {statement.Kind}");
+                        }
+                    }
+                }
+
+                blocks.Insert(0, _start);
+                blocks.Add(_end);
+
+                return new ControlFlowGraph(_start, _end, blocks, _branches);
+            }
+
+            private BoundExpression Negate(BoundExpression condition)
+            {
+                if (condition is BoundLiteralExpression literal)
+                {
+                    var value = (bool)literal.Value;
+                    return new BoundLiteralExpression(!value);
+                }
+                var op = BoundUnaryOperator.Bind(SyntaxKind.BangToken, TypeSymbol.Bool);
+                return new BoundUnaryExpression(op, condition);
             }
 
             private void Connect(BasicBlock from, BasicBlock to, BoundExpression condition = null)
             {
+                if (condition is BoundLiteralExpression l)
+                {
+                    var value = (bool)l.Value;
+                    if (value)
+                        condition = null;
+                    else
+                        return;
+                }
+
                 var branch = new BasicBlockBranch(from, to,condition);
                 from.Outgoing.Add(branch);
                 to.Incoming.Add(branch);
@@ -150,7 +223,7 @@ namespace Ncodi.CodeAnalysis.Binding
         public void WriteTo(TextWriter writer)
         {
 
-           string Quote(string text)
+            string Quote(string text)
             {
                 return "\"" + text.Replace("\"", "\\\"") + "\"";
             }
