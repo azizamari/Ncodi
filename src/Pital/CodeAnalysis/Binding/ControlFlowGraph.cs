06f6c4f8ccd3162ff00f2e0175bbbdd964fe1ae5
azizamari <azizamari2003@gmail.com>
2021-05-10 16:01:25 +0100

    Fix exception when declaring empty function that has a return type

--- src/Pital/CodeAnalysis/Binding/ControlFlowGraph.cs
+++ src/Pital/CodeAnalysis/Binding/ControlFlowGraph.cs
@@ -295,8 +295,8 @@ namespace Ncodi.CodeAnalysis.Binding
 
             foreach (var branch in graph.End.Incoming)
             {
-                var lastStatement = branch.From.Statements.Last();
-                if (lastStatement.Kind != BoundNodeKind.ReturnStatement)
+                var lastStatement = branch.From.Statements.LastOrDefault();
+                if (lastStatement == null || lastStatement.Kind != BoundNodeKind.ReturnStatement)
                     return false;
             }
 
