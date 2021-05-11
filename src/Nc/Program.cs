f3049873dbebf6d1d4be026918ea36f0b822e1d4
azizamari <azizamari2003@gmail.com>
2021-05-11 19:59:20 +0100

    initial code for getting code from files instead of repl

--- src/Nc/Program.cs
+++ src/Nc/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Ncodi.CodeAnalysis;
+using Ncodi.CodeAnalysis.Symbols;
+using Ncodi.CodeAnalysis.Syntax;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-namespace Nc
+namespace Ncodi
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Usage: nc <Source-Path");
+            }
+            if (args.Length > 1)
+            {
+                Console.Error.WriteLine("Error: only one path supported for the time being");
+            }
+
+            var path = args.Single();
+            var text = File.ReadAllText(path);
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            var compilation = new Compilation(syntaxTree);
+            compilation.Evaluate(new Dictionary<VariableSymbol, object>());
         }
     }
 }
