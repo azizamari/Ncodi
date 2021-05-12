32db5e49b3b1e0dea52ef1a4deebeb1ce8cfbbc4
azizamari <azizamari2003@gmail.com>
2021-05-12 12:44:26 +0100

    Use try catch to pervent exception when passing invalid path

--- src/Nc/Program.cs
+++ src/Nc/Program.cs
@@ -14,19 +14,29 @@ namespace Ncodi
         {
             if (args.Length == 0)
             {
-                Console.Error.WriteLine("Usage: nc <Source-Path");
+                Console.Error.WriteLine("Usage: nc <Source-Path>");
+                return;
             }
             if (args.Length > 1)
             {
                 Console.Error.WriteLine("Error: only one path supported for the time being");
+                return;
             }
 
             var path = args.Single();
-            var text = File.ReadAllText(path);
-            var syntaxTree = SyntaxTree.Parse(text);
+            try
+            {
+                var text = File.ReadAllText(path);
+                var syntaxTree = SyntaxTree.Parse(text);
 
-            var compilation = new Compilation(syntaxTree);
-            compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+                var compilation = new Compilation(syntaxTree);
+                var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+            }
+            catch (IOException)
+            {
+                Console.Error.WriteLine("Error: File Not Found");
+                return;
+            }
         }
     }
 }
