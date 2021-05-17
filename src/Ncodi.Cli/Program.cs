097bb57b22bb42ed7702b9a67bdd33d5488b75ce
azizamari <azizamari2003@gmail.com>
2021-05-17 12:34:49 +0100

    Add new project for ncodi CLI

---
+++ src/Ncodi.Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ncodi.Cli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var helloWorld = await GetHelloWorldAsync();
+            Console.WriteLine(helloWorld);
+        }
+
+        static Task<string> GetHelloWorldAsync()
+        {
+            return Task.FromResult("Hello Async World");
+        }
+    }
+}
