aab0d32e1f36e94f9e88b5db67efa57b27b079bd
azizamari <azizamari2003@gmail.com>
2021-05-18 22:07:25 +0100

    Lex slashslash token "//"

--- src/Ncodi/CodeAnalysis/Syntax/Lexer.cs
+++ src/Ncodi/CodeAnalysis/Syntax/Lexer.cs
@@ -59,10 +59,6 @@ namespace Ncodi.CodeAnalysis.Syntax
                     _kind = SyntaxKind.MinusToken;
                     _position++;
                     break;
-                case '/':
-                    _kind = SyntaxKind.SlashToken;
-                    _position++;
-                    break;
                 case '%':
                     _kind = SyntaxKind.ModuloToken;
                     _position++;
@@ -98,6 +94,18 @@ namespace Ncodi.CodeAnalysis.Syntax
                 case ':':
                     _kind = SyntaxKind.ColonToken;
                     _position++;
+                    break;
+                case '/':
+                    _position++;
+                    if (Current != '/')
+                    {
+                        _kind = SyntaxKind.SlashToken;
+                    }
+                    else
+                    {
+                        _kind = SyntaxKind.SlashSlashToken;
+                        _position++;
+                    }
                     break;
                 case '*':
                     _position++;
--- src/Ncodi/CodeAnalysis/Syntax/SyntaxKind.cs
+++ src/Ncodi/CodeAnalysis/Syntax/SyntaxKind.cs
@@ -81,5 +81,6 @@
         ReturnStatement,
         StarStarToken,
         DecimalToken,
+        SlashSlashToken,
     }
 }
