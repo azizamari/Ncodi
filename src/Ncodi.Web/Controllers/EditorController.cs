32e5bb89ca9e8c44bcc83b5a0e606eabea63aa3f
azizamari <azizamari2003@gmail.com>
2021-05-23 11:32:13 +0100

    Pass code using web api

---
+++ src/Ncodi.Web/Controllers/EditorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ncodi.CodeAnalysis;
+using Ncodi.CodeAnalysis.IO;
+using Ncodi.CodeAnalysis.Symbols;
+using Ncodi.CodeAnalysis.Syntax;
+using Ncodi.CodeAnalysis.Text;
+using Ncodi.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ncodi.Web.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class EditorController: ControllerBase
+    {
+        [HttpPost("{controller}/run")]
+        public IActionResult Run(CodeDto code)
+        {
+            var srouce = SourceText.From(code.Code, "file.ncodi");
+            var syntaxTree = SyntaxTree.Parse(srouce);
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+            if (!result.Diagnostics.Any())
+            {
+                if (result.Value != null)
+                    Console.Out.WriteLine(result.Value);
+            }
+            else
+            {
+                Console.WriteLine("Error List:");
+                Console.Error.WriteDiagnostics(result.Diagnostics);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ src/Ncodi.Web/Models/CodeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ncodi.Web.Models
+{
+    public class CodeDto
+    {
+        public string Code { get; set; }
+        public string FileNmae { get; set; }
+    }
+}
