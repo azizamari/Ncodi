297b7e6d1a2d737f354b1db4a6b1b13057f0f823
azizamari <azizamari2003@gmail.com>
2021-05-27 17:04:52 +0100

    add ncodi-website sections to home endpoint

---
+++ src/Ncodi.Web/wwwroot/js/animation.js
@@ -0,0 +1,20 @@
+﻿const questions = document.querySelectorAll(".accordion_item")
+questions.forEach(question => question.addEventListener("click", () => {
+    const currentAnswer = question.querySelector(".answer");
+    if (currentAnswer.classList.contains("openAnswer")) {
+        currentAnswer.classList.remove("openAnswer");
+        question.querySelector(".accordion_link .bx-plus").style.display = "block";
+        question.querySelector(".accordion_link .bx-minus").style.display = "none";
+    }
+    else {
+        let open = document.querySelectorAll(".openAnswer");
+        if (open.length !== 0) {
+            open[0].classList.remove("openAnswer");
+            open[0].parentElement.querySelector(".accordion_link .bx-plus").style.display = "block";
+            open[0].parentElement.querySelector(".accordion_link .bx-minus").style.display = "none";
+        }
+        currentAnswer.classList.add("openAnswer");
+        question.querySelector(".accordion_link .bx-plus").style.display = "none";
+        question.querySelector(".accordion_link .bx-minus").style.display = "block";
+    }
+}));
