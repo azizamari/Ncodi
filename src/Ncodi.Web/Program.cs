fcf2f3f10e14c532ac83e52a25e1bda8c98843e3
Aziz Amari <azizamari2003@gmail.com>
2021-07-18 11:27:23 +0100

    Add raouf ghrissi testimonial

--- src/Ncodi.Web/Program.cs
+++ src/Ncodi.Web/Program.cs
@@ -21,9 +21,9 @@ namespace Ncodi.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    //webBuilder.ConfigureKestrel(serverOptions=>{
-                    //   serverOptions.Listen(IPAddress.Any,Convert.ToInt32(Environment.GetEnvironmentVariable("PORT")));
-                    //}).UseStartup<Startup>();
+                    // webBuilder.ConfigureKestrel(serverOptions=>{
+                    //     serverOptions.Listen(IPAddress.Any,Convert.ToInt32(Environment.GetEnvironmentVariable("PORT")));
+                    // }).UseStartup<Startup>();
                     webBuilder.UseStartup<Startup>();
                 });
     }
